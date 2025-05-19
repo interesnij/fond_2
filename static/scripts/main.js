58e31f3c129d9262fa25fcbc02c1fd6160b0e618
interesnij <ochkarik1983@mail.ru>
2025-05-19 20:05:37 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2542,8 +2542,9 @@ $(window).scroll(updateMouseOffset),
 $(window).on("resize", function() {
   fo_canvas = document.body.querySelector(".fo-canvas");
     $("body").hasClass("popup-capsule-opened") && (height < fo_canvas.innerHeight()
-                                                   || width < fo_canvas.innerWidth()
-                                                   || !isTouchDevice()) && fo_init(!0)
+                                                   //|| width < fo_canvas.innerWidth()
+                                                   //|| !isTouchDevice()
+                                                ) && fo_init(!0)
 });
 
 
